6ed39cc4c991e610ac46bf9602e16ebaa79071ca
Tiernjo <Tiernjo.gmail.com>
2014-02-03 23:43:56 -0600

    boop

--- src/grid.rs
+++ src/grid.rs
@@ -2,61 +2,6 @@ extern mod rsfml;
 use rsfml::graphics::{Color, RectangleShape};
 use rsfml::system::{Vector2f};
 
-struct GridSquare {
-	is_empty: ~bool,
-	is_x: ~bool
-}
-
-impl GridSquare {
-	fn block (&self) -> RectangleShape {
-		let block = match RectangleShape::new() {
-			Some(block)	=> block,
-			None() 		=> fail!(~"Error, creating block."),
-		};
-		block
-	}
-}
-
-
-
-pub fn create_regions(which: int) -> (GridSquare, RectangleShape){
-	let width:f32 = 900.0;
-	let height:f32 = 600.0;
-
-	// Vector2f of each needed coordinate point set
-	let zero_zero:Vector2f = Vector2f::new(0.0, 0.0);
-	let third_zero:Vector2f = Vector2f::new(width/3.0, 0.0);
-	let two_zero:Vector2f = Vector2f::new(width * 2.0 / 3.0, 0.0);
-	let zero_third:Vector2f = Vector2f::new(0.0, height/3.0);
-	let two_third:Vector2f = Vector2f::new(width * 2.0/3.0, height/3.0);
-	let zero_two:Vector2f = Vector2f::new(0.0, height * 2.0 / 3.0);
-	let third_two:Vector2f = Vector2f::new(width / 3.0, height * 2.0 / 3.0);
-	let two_two:Vector2f = Vector2f::new(width * 2.0 / 3.0, height * 2.0 / 3.0);
-
-	// Size of each grid
-	let third_third:Vector2f = Vector2f::new(width/3.0, height/3.0);
-
-	// Create each region
-	let grid_bool:GridSquare = GridSquare{is_x: ~false, is_empty: ~true};
-	let mut block = grid_bool.block();
-
-	// Assign position, size, and color of block depending on which block is being made
-	match which {
-		0	=>	{block.set_position(&zero_zero); block.set_size(&third_third); block.set_fill_color(&Color::red());}	// top_left
-		1	=>	{block.set_position(&third_zero); block.set_size(&third_third); block.set_fill_color(&Color::white());}	// top_mid
-		2	=>	{block.set_position(&two_zero); block.set_size(&third_third); block.set_fill_color(&Color::red());}		// top_right
-		3	=>	{block.set_position(&zero_third); block.set_size(&third_third); block.set_fill_color(&Color::white());}	// mid_left
-		4	=>	{block.set_position(&third_third); block.set_size(&third_third); block.set_fill_color(&Color::red());}	// mid_mid
-		5	=>	{block.set_position(&two_third); block.set_size(&third_third); block.set_fill_color(&Color::white());}	// mid_right
-		6   =>	{block.set_position(&zero_two); block.set_size(&third_third); block.set_fill_color(&Color::red());}		// bot_left
-		7	=>	{block.set_position(&third_two); block.set_size(&third_third); block.set_fill_color(&Color::white());}	// bot_mid
-		8	=>	{block.set_position(&two_two); block.set_size(&third_third); block.set_fill_color(&Color::red());}		// bot_right
-		_	=>	fail!("Error, cannot initialize {}'s block.", which + 1)
-	}
-
-	(grid_bool, block)
-}
-
 pub fn create_lines() -> (RectangleShape, RectangleShape, RectangleShape, RectangleShape){
 	// window width and height
 	let width:f32 = 900.0;
--- src/run.rs
+++ src/run.rs
@@ -1,5 +1,5 @@
 extern mod rsfml;
-use rsfml::graphics::{RectangleShape, RenderWindow, Color};
+use rsfml::graphics::{Color, RectangleShape, RenderWindow};
 
 mod control;
 mod grid;
@@ -12,49 +12,28 @@ pub fn main_loop() {
 	// Get Window
 	let mut window = ::window::create();
 
-	// Get Top Row of Grid
-	let (top_left_bool, top_left) = ::grid::create_regions(0);
-	let (top_mid_bool, top_mid) = ::grid::create_regions(1);
-	let (top_right_bool, top_right) = ::grid::create_regions(2);
-	// Get Mid Row of Grid
-	let (mid_left_bool, mid_left) = ::grid::create_regions(3);
-	let (mid_mid_bool, mid_mid) = ::grid::create_regions(4);
-	let (mid_right_bool, mid_right) = ::grid::create_regions(5);
-	// Get Bot Row of Grid
-	let (bot_left_bool, bot_left) = ::grid::create_regions(6);
-	let (bot_mid_bool, bot_mid) = ::grid::create_regions(7);
-	let (bot_right_bool, bot_right) = ::grid::create_regions(8);
-
 	// Get Lines
 	let (left_line, right_line, top_line, bottom_line) = ::grid::create_lines();
-
+	
 	while window.is_open() {
 		::control::input(&mut window);
 		show(&mut window, 
-			&top_left, &top_mid, &top_right, 
-			&mid_left, &mid_mid, &mid_right, 
-			&bot_left,	&bot_mid, &bot_right,
 			&left_line, &right_line, &top_line, &bottom_line);
 	}
 }
 
 // Clear buffer, draw items, show results
-fn show(window: &mut RenderWindow, 
-	top_left: &RectangleShape, top_mid: &RectangleShape, top_right: &RectangleShape, 
-	mid_left: &RectangleShape, mid_mid: &RectangleShape, mid_right: &RectangleShape, 
-	bot_left: &RectangleShape, bot_mid: &RectangleShape, bot_right: &RectangleShape,
+fn show(window: &mut RenderWindow,
 	left_line: &RectangleShape, right_line: &RectangleShape, 
 	top_line: &RectangleShape, bottom_line: &RectangleShape) {
 
 	// Clean out window
-	window.clear(&Color::white());
-	// Draw grid_regions
-	window.draw(top_left); window.draw(top_mid); window.draw(top_right);
-	window.draw(mid_left); window.draw(mid_mid); window.draw(mid_right);
-	window.draw(bot_left); window.draw(bot_mid); window.draw(bot_right);
+	window.clear(&Color::red());
+
 	// Draw grid_lines
 	window.draw(left_line); window.draw(right_line); 
 	window.draw(top_line); window.draw(bottom_line);
+
 	// Display window
 	window.display()
 }
